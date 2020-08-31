fa004e8aa47002941a64d1b5f94590e0b9e8a057
BilalGHULAM <bilal.ghulam.0123@gmail.com>
2020-08-31 18:27:34 +0200

    add page participated event + add page created event

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,23 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    /**
+     * @Route("/", name="list_envents_user_created")
+     */
+    public function list_envent_user_created()
+    {
+        return $this->render('home/page_events_user_created.html.twig', [
+            'controller_name' => 'Liste des evenements des utilisateurs',
+        ]);
+    }
+    /**
+     * @Route("/", name="list_envents_user_participated")
+     */
+    public function list_envent_user_participated()
+    {
+        return $this->render('home/page_events_user_paticipated.html.twig', [
+            'controller_name' => 'Liste des evenements des utilisateurs',
+        ]);
+    }
 }
