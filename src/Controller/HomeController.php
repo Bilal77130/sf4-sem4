be31ecdcaa9e9388f38e45c6101f7ae2e2698a03
BilalGHULAM <bilal.ghulam.0123@gmail.com>
2020-09-03 14:24:34 +0200

    debug page event

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -250,6 +250,8 @@ class HomeController extends AbstractController
 
         $event = $repository->find($request->get('id'));
 
+        dd($event);
+
         if( $this->security->getUser() ){ 
             if (!in_array('ROLE_ADMIN', $this->security->getUser()->getRoles())) {
                 if (!$this->isGranted('POST_EDIT', $event)) {
